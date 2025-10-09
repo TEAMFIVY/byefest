65c6f6029733ed19bfd04d33a4c1ff88070a636a
Davin Amanta <davin16112007@gmail.com>
2025-10-09 21:41:08 +0700

    fix dashboard

--- application/controllers/Dashboard.php
+++ application/controllers/Dashboard.php
@@ -1,15 +1,7 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Dashboard extends CI_Controller {
-	public function __construct() {
-        parent::__construct();
-        if (!$this->session->userdata('user_id')) {
-            $this->session->set_flashdata('error', 'Anda harus login terlebih dahulu.');
-            redirect('auth/login');
-        }
-    }
-	
+class Dashboard extends CI_Controller {	
 	public function index()
 	{
 		$this->load->view('dashboard');
